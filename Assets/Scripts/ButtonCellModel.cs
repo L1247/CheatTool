af87f93b8a4f91322498f1973338476d75984fdc
r Star <starlove124760@gmail.com>
2023-01-29 01:34:40 +0800

    feat: outline color to red onButtonSelected

--- Assets/Scripts/ButtonCellModel.cs
+++ Assets/Scripts/ButtonCellModel.cs
@@ -2,18 +2,39 @@
 
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 #endregion
 
 namespace CheatTool
 {
-    public class ButtonCellModel : MonoBehaviour , ISelectHandler
+    public class ButtonCellModel : MonoBehaviour , ISelectHandler , IDeselectHandler
     {
+    #region Private Variables
+
+        private Outline outline;
+
+    #endregion
+
+    #region Unity events
+
+        private void Awake()
+        {
+            outline = GetComponent<Outline>();
+        }
+
+    #endregion
+
     #region Public Methods
+
+        public void OnDeselect(BaseEventData eventData)
+        {
+            outline.effectColor = Color.black;
+        }
 
         public void OnSelect(BaseEventData eventData)
         {
-            Debug.Log($"{gameObject.name}");
+            outline.effectColor = new Color(0.85f , 0.07f , 0.21f , 1);
         }
 
     #endregion
