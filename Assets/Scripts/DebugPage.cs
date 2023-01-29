69385a6f860728631708248c78dd9a17671b00f9
r Star <starlove124760@gmail.com>
2023-01-29 21:45:42 +0800

    remove update

--- Assets/Scripts/DebugPage.cs
+++ Assets/Scripts/DebugPage.cs
@@ -8,22 +8,6 @@ namespace CheatTool
 {
     public class DebugPage : PageBase
     {
-    #region Unity events
-
-        protected override void Update()
-        {
-            base.Update();
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                // foreach (var button in buttons)
-                // {
-                //     EventSystem.current.SetSelectedGameObject(button.gameObject);
-                // }
-            }
-        }
-
-    #endregion
-
     #region Protected Methods
 
         protected override void Initialization()
